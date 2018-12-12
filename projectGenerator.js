7ebe42e83352b3d982ec10895290e38b4b82bf17
tcje91 <tcje9117@outlook.com>
2018-12-12 17:17:46 +0000

    working generator

--- projectGenerator.js
+++ projectGenerator.js
@@ -1,26 +1,42 @@
+#!/usr/bin/env node
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-fs.mkdir("./myProject", error => {
+fs.mkdir(`${process.cwd()}/myProject`, error => {
   if (error) throw error;
-  console.log("Project Folder written");
+  console.log("project folder created");
 
-  fs.writeFile("myProject/index.js", "// sample code", "utf8", error => {
+  fs.writeFile(`${process.cwd()}/myProject/index.js`, "// sample code", "utf8", error => {
     if (error) throw error;
-    console.log("Index File written");
+    console.log("index file written");
   });
-  fs.mkdir("./myProject/spec", error => {
+  fs.mkdir(`${process.cwd()}/myProject/spec`, error => {
     if (error) throw error;
-    console.log("Spec Folder written");
+    console.log("spec folder created");
 
     fs.writeFile(
-      "myProject/spec/index.spec.js",
+      `${process.cwd()}/myProject/spec/index.spec.js`,
       "// sample tests",
       "utf8",
       error => {
         if (error) throw error;
-        console.log("Spec File written");
+        console.log("spec file written");
       }
     );
   });
+  fs.writeFile(`${process.cwd()}/myProject/.gitignore`, "node_modules", error => {
+    if (error) throw error;
+    console.log("gitignore file written");
+  });
+  fs.readFile(
+    "./sampleFiles/packageSample.json",
+    (error, fileContents) => {
+      if (error) console.log(error);
+      fs.writeFile(`${process.cwd()}/myProject/package.json`, fileContents, error => {
+        if (error) console.log(error);
+        console.log("package.json file written");
+      });
+    }
+  );
 });
